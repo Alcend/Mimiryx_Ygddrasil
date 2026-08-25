0f00fdaa8bd7d6b8593e0453371e0241a38eaa63
Julius <152962765+Alcend@users.noreply.github.com>
2026-08-25 11:16:04 +0800

    chore: add ESLint & Prettier + CI lint step

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import reactPlugin from 'eslint-plugin-react';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true }
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      'react': reactPlugin
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-undef': 'off',
+      'no-empty': ['error', { 'allowEmptyCatch': true }],
+      'no-useless-escape': 'off',
+      'react/react-in-jsx-scope': 'off'
+    }
+  }
+];
